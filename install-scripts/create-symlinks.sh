b8fd6006bf54668a66584cc7e4cf4754c442975e
BitmexTrader <bitcointraderbyday@gmail.com>
2019-12-13 18:14:32 +1100

    Added install file

---
+++ install-scripts/create-symlinks.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+dotfiles_dir=/Desktop/MyDotfiles
+log_file=/install_progress_log.txt
---
+++ install-scripts/install_packages.sh
@@ -0,0 +1,8 @@
+log_file=/install_progress_log.txt
+
+sudo apt-get -y install zsh
+if type -p zsh > /dev/null; then
+    echo "zsh Installed" >> $log_file
+else
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+fi
