baa52294c6e9aca0a8bc908df1602ce3a8c57eac
BitmexTrader <bitcointraderbyday@gmail.com>
2019-12-14 12:40:47 +1100

    Added a file for alias

---
+++ .zsh_files/aliases.zsh
@@ -0,0 +1,48 @@
+# ALIASES
+##########################################
+# zsh/oh-my-zsh aliasies
+##########################################
+alias zshconfig="nvim ~/.zshrc"
+alias ohmyzsh="nvim ~/.oh-my-zsh"
+alias p10k="nvim ~/.zsh_files/.p10k.zsh"
+##########################################
+# Vim to Nvim
+##########################################
+alias vi="nvim"
+alias vim="nvim"
+
+##########################################
+#CONFIGS/DOTFILES
+##########################################
+#Neovim Config File
+alias int="nvim ~/.config/nvim/init.vim"
+#tmux Config File 
+alias tconfig="nvim ~/.tmux.conf"
+
+##########################################
+# Desktop alias
+##########################################
+alias CB="~/Desktop/CookBooks"
+alias todo="nvim ~/Desktop/TODOS/ToDayToDo.txt"
+alias TD="~/Desktop/TODOS"
+
+##########################################
+#Repositories
+##########################################
+#Github Dotfiles README.md
+alias Dotme="nvim ~/Desktop/MyDotfiles/README.md"
+alias Dotdir="~/Desktop/MyDotfiles"
+##########################################
+# Easier Navigation aliases
+##########################################
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+##########################################
+# Open Neovim Pluging Dir aliases
+##########################################
+alias P="cd ~/.config/nvim/plugged"
+
+
